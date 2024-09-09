cdb7899a721f1298bed00b6b20f052e76228770d
Xe_131 <2730252814@qq.com>
2024-09-09 15:49:54 +0800

    将waveform 时域信号在任务中的公共区域从全局变量改为队列

--- ESP_Arduino_learn/formal_code/formal_code.ino
+++ ESP_Arduino_learn/formal_code/formal_code.ino
@@ -9,8 +9,11 @@
 #define FREQENCENUM 129 // 单窗的频率分量个数，WINDOWSIZE/2 为有效量，轴对称
 #define SCALE 10000 // 将缩放到1 到 -1 的原始信号二次缩放
 
-// 时域信号
-double* waveform = NULL;
+// 时域信号(队列)
+StaticQueue_t waveform_QueueControlBlock;
+QueueHandle_t xQueue_waveform;
+uint8_t* waveform = NULL;
+#define wave_queue_timeout 17
 // 频域信号
 double* spectrogram = NULL;
 
@@ -36,6 +39,22 @@ void setup() {
   if(xMutexInventory_1 == NULL){
     Serial.println("No Enough Ram For Mutex");
   }
+  xMutexInventory_2 = xSemaphoreCreateMutex();
+  if(xMutexInventory_2 == NULL){
+    Serial.println("No Enough Ram For Mutex");
+  }
+  // 全局变量的内存分配
+  waveform = (uint8_t*)ps_malloc(WINDOWSTEP*2 * sizeof(double)); // 时域信号
+  spectrogram = (double*)ps_malloc(WINDOWNUM*FREQENCENUM * sizeof(double)); // 频域信号
+  xQueue_waveform = xQueueCreateStatic(WINDOWSTEP*2, sizeof(double), waveform, &waveform_QueueControlBlock);
+  if(xQueue_waveform != NULL){
+    Serial.println("waveform 队列创建成功");
+  }
+  else{
+    Serial.println("waveform 队列创建失败");
+  }
+
+
   // 创建任务(core 1)
   xTaskCreatePinnedToCore(task1, "iis mic", 1024*3, NULL, 1, NULL, 1);
   vTaskDelay(30); // 先让mic 采集数据
@@ -54,15 +73,13 @@ void task1(void* parameters){
   I2SClass I2S;
   int sample = 0;
   double scaledSample = 0;
-  // 分配到SRAM
-  double* waveform_copy = (double*)ps_malloc(WINDOWSTEP * sizeof(double));
-  waveform = (double*)ps_malloc(WINDOWSTEP * sizeof(double));
+
   // 初始化mic
   I2S.setPins(41, 42, -1, 2, -1);  //SCK, WS, SDOUT, SDIN, MCLK
   I2S.begin(I2S_MODE_STD, 16000, I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_MONO, -1);
 
   while(1){
-    // 采集信号到任务私有数组
+    // 采集数据并做缩放
     for(int i = 0; i < WINDOWSTEP; i++){
       while(1){
         sample = I2S.read();
@@ -72,19 +89,12 @@ void task1(void* parameters){
       }
       sample >>= 8;
       scaledSample = (sample / 8388607.0)*SCALE; // 2^23 - 1 = 8388607
-      waveform_copy[i] = scaledSample;
-    }
-
-    // 将数据传到公共区域
-    if(xSemaphoreTake(xMutexInventory_1, timeout) == pdPASS){
-      for(int i = 0; i < WINDOWSTEP; i++){
-        waveform[i] = waveform_copy[i];
+      // 向队列发送数据
+      if(xQueueSend(xQueue_waveform, &scaledSample, wave_queue_timeout) == pdPASS){
       }
-      // 释放钥匙
-      xSemaphoreGive(xMutexInventory_1);     
-    }
-    else{
-      Serial.println("mic写数据 未获得钥匙");
+      else{
+        Serial.printf("waveform 队列已满超过 %d ms\n", wave_queue_timeout);
+      }
     }
 
   }
@@ -98,25 +108,24 @@ void task2(void* parameters){
   double* vImag = (double*)ps_malloc(WINDOWSIZE * sizeof(double));
   // 寄存一个window 的时域信号
   double* window_wave = (double*)ps_malloc(WINDOWSIZE * sizeof(double));
-  // 频域信号（公共）
-  spectrogram = (double*)ps_malloc(WINDOWNUM*FREQENCENUM * sizeof(double));
-
+  // 临时寄存128 个原始信号
+  double* window_temp = (double*)ps_malloc(WINDOWSTEP * sizeof(double));
   ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, WINDOWSIZE, 16000);
 
   while(1){
  
+    // 更新时域信号到window_wave(使用队列)
+    buffer_shift(xQueue_waveform, window_wave, WINDOWSTEP, WINDOWSIZE, wave_queue_timeout);
 
-    // 初始化虚数
-    for(int i = 0; i < WINDOWSIZE; i++){
-      vImag[i] = 0;
-    }
-    // 更新时域信号到window_wave
-    buffer_update(waveform, window_wave, WINDOWSTEP, WINDOWSIZE, &xMutexInventory_1);
     // 复制到vReal
     for(int i = 0; i < WINDOWSIZE; i++){
       vReal[i] = window_wave[i];
     }
 
+    // 初始化虚数
+    for(int i = 0; i < WINDOWSIZE; i++){
+      vImag[i] = 0;
+    }
  
 
     // 进行FFT 操作
@@ -125,17 +134,17 @@ void task2(void* parameters){
     FFT.complexToMagnitude();
     
     // 更新公共频谱图（数据移位）
-    buffer_update(vReal, spectrogram, WINDOWSIZE, WINDOWNUM*FREQENCENUM, &xMutexInventory_1);
+    buffer_update(vReal, spectrogram, WINDOWSIZE, WINDOWNUM*FREQENCENUM, &xMutexInventory_2);
     
 
 
-    Serial.println("\n\n\n\n\n");
-    // 打印频谱图
-    for(int i = 0; i < WINDOWNUM*FREQENCENUM; i++){
-      if(i % 100 == 0){
-        Serial.printf("i = %d %4f\n", i, spectrogram[i]);
-      }
-    }     
+    // Serial.println("\n\n\n\n\n");
+    // // 打印频谱图
+    // for(int i = 0; i < WINDOWNUM*FREQENCENUM; i++){
+    //   if(i % 100 == 0){
+    //     Serial.printf("i = %d %4f\n", i, spectrogram[i]);
+    //   }
+    // }     
     
  
 
@@ -171,4 +180,32 @@ void buffer_update(double* from, double* to, int from_size, int to_size, Semapho
     Serial.println("buffer_update 未获得钥匙");
   }
 
+}
+
+void buffer_shift(QueueHandle_t queue, double* buffer, int shift_size, int buffer_size, int queue_timeout){
+  if(shift_size >= buffer_size){
+    Serial.println("buffer_shift went wrong !");
+    return;
+  }
+  
+  // 转移自己内存
+  for(int i = shift_size; i < buffer_size; i++){
+    buffer[i-shift_size] = buffer[i];
+  }  
+
+  // 从队列中添加新的数据
+  while(1){
+    if(xQueueReceive(queue, (buffer+buffer_size-1), queue_timeout) == pdPASS){
+    }
+    else{
+      Serial.printf("buffer shift 队列已空超过 %d ms\n", queue_timeout);
+    }
+
+    buffer_size--;
+    shift_size--;
+    if(shift_size == 0){
+      break;
+    }
+    
+  }
 }
